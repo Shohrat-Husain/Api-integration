114411aaeb2b14a4b2e82d0eacc767ad49040616
Mohd Shohrat <mshohrat@pokerbaazi.in>
2022-10-06 13:23:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getApi = async () => {
+    const response = await fetch("https://api.github.com/users");
+    setUsers(await response.json());
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" container abc">
+      {users.map((curElem) => {
+        return (
+          <div className="my-5 col-md-4 text-center  ">
+            <div className="card   " style={{ width: "18rem" }}>
+              <img
+                src={curElem.avatar_url}
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
